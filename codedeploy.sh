852e36335ccd9cdfe7bbfe75753a8e47e86025a4
Armando <aalorro@gmail.com>
2020-05-21 21:29:36 -0400

    modified script

--- codedeploy.sh
+++ codedeploy.sh
@@ -5,7 +5,5 @@ sudo apt -y install ruby
 cd /tmp
 sudo wget https://aws-codedeploy-us-east-2.s3.amazonaws.com/latest/install
 sudo chmod +x ./install
-
 sudo ./install auto
-
 sudo service codedeploy-agent start
