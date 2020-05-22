86729792b1c36f074e0181e06e1340d71ce3e3de
Armando <aalorro@gmail.com>
2020-05-21 22:34:45 -0400

    modified code deploy script

--- codedeploy.sh
+++ codedeploy.sh
@@ -1,7 +1,10 @@
-#!/usr/bin
+#!/bin/bash
 
 sudo apt update
-sudo apt -y install ruby
+sudo apt install apache2
+sudo apt install ruby -y
+sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) universe"
+sudo apt-get install gdebi-core -y
 cd /tmp
 sudo wget https://aws-codedeploy-us-east-2.s3.amazonaws.com/latest/install
 sudo chmod +x ./install
