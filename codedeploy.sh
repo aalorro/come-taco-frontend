ee8f508f1f44cfc19e23bd3943460f81601c71c4
Armando <aalorro@gmail.com>
2020-05-26 11:28:20 -0400

    new edit

--- codedeploy.sh
+++ codedeploy.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get update
 sudo apt-get install apache2 -y
-sudo apt=get install ruby -y
+sudo apt-get install ruby -y
 sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) universe"
 sudo apt-get install gdebi-core -y
 cd /tmp
